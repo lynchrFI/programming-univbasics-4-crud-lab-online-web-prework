5fd94d40346c220d599623564037148efee4e95a
Ryan Lynch <lynchrbusiness@gmail.com>
2019-09-28 03:07:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,33 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+muscle_cars = ["Mustang", "Camaro", "Charger", "Corvette"]
   
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  muscle_cars = ["Mustang", "Camaro", "Charger", "Corvette"]
+  muscle_cars<< "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  muscle_cars = ["Mustang", "Camaro", "Charger", "Corvette"]
+  muscle_cars.unshift("wow")
+  p muscle_cars
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+ array.index
 end
 
 def retrieve_first_element_from_array(array)
